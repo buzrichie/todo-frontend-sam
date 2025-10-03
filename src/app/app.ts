f926673a63dd1e2ae86faa005979419bf757365d
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-10-03 23:58:02 +0000

    fix: ssr on deploy #2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AmplifyService } from './services/amplify-service';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AmplifyAuthenticatorModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App implements OnInit {
   protected readonly title = signal('todo-frontend-sam');
