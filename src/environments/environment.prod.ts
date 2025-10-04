ad37a5241288f572aec8c09d2cfe918370a7582b
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-10-04 02:08:48 +0000

    fix deploy #1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://4qodzzvv5b.execute-api.eu-central-1.amazonaws.com/dev',
+  cognito: {
+    userPoolId: 'eu-central-1_B0WehpaKy',
+    userPoolWebClientId: '46qknu5n4rnfrgqoq1gts30u05',
+    region: 'eu-central-1',
+  },
+};
